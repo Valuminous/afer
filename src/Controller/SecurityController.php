da8f4f8b202b40768777fb70de41f8b53bb3351d
elodie-martin <elodie.m@codeur.online>
2019-02-12 15:47:25 +0000

    mise en place inscription utilisateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class SecurityController extends AbstractController
 {
@@ -28,8 +33,31 @@ class SecurityController extends AbstractController
      * @Route("/logout", name="logout")
      */
     
-    public function logout()
-    {
+    public function logout(){}
+
+    /**
+     * @Route("/admin/inscription", name="registration")
+     */
+    public function registration(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder) {
+        $user = new User();
+
+        $form = $this->createForm(RegistrationType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+
+            $user->setPassword($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+        return $this->render('security/registration.html.twig', [
+            'formRegistration' => $form->createView(),
+            'editMode' => $user->getId()
+        ]);
     }
 }
 
