1abe2fec9eb0be1b7cf8c76c8e39d3dda2e826db
abdelkrimn <abdelkrim.n@codeur.online>
2019-02-18 15:59:59 +0000

    fusion master

--- src/Controller/StageController.php
+++ src/Controller/StageController.php
@@ -3,13 +3,23 @@
 namespace App\Controller;
 
 use App\Entity\Stage;
+use App\Entity\Tribunal;
+use App\Entity\Prefecture;
+
 use App\Form\StageType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Form\TribunalType;
+use App\Form\PrefectureType;
+
+use App\Repository\StageRepository;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Doctrine\Common\Persistence\ObjectManager;
-use App\Repository\StageRepository;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/admin")
@@ -28,7 +38,97 @@ class StageController extends AbstractController
             'stages' => $stages
         ]);
     }
- /**
+
+     /**
+     *  @Route("/stage/loadFormTribunal", name="stage_tribunal")
+     */
+    public function popTribunal(Tribunal $tribunal = null, Request $request, ObjectManager $manager)
+    {
+        if(!$tribunal){
+            $tribunal = new Tribunal();
+        }
+
+        $form = $this->createForm( TribunalType::class, $tribunal, array('method'=>'POST'));
+        $form->handleRequest( $request );
+
+        if($request->isMethod('POST')){
+            $tribunalNom = $request->request->get('tribunal_nom_tribunal');
+            $tribunalAdresse = $request->request->get('tribunal_adresse_tribunal');
+            $tribunalNumeroAdresse = $request->request->get('tribunal_numero_adresse_tribunal');
+            $tribunalCommune = $request->request->get('tribunal_commune_tribunal');
+            $tribunalAutorite = $request->request->get('tribunal_tribunal_service');
+            $tribunalService = $request->request->get('tribunal_tribunal_autorite');
+      
+            if(strlen($tribunalNom) > 0 && strlen($tribunalAdresse) > 0 && strlen($tribunalNumeroAdresse) > 0 &&
+                strlen($tribunalCommune) > 0 && strlen($tribunalAutorite) > 0 && strlen($tribunalService) > 0){
+                
+                $tribunal->setNomTribunal($tribunalNom);
+                $tribunal->setAdresseTribunal($tribunalAdresse);
+                $tribunal->setNumeroAdresseTribunal($tribunalNumeroAdresse);
+                $tribunal->setCommuneTribunal($tribunalCommune);
+                $tribunal->getTribunalService($tribunalAutorite);
+                $tribunal->getTribunalAutorite($tribunalService);
+                $manager->persist( $tribunal );
+                $manager->flush();
+                $response = new Response();
+                $response = JsonResponse::fromJsonString('{"id":'.$tribunal->getId().', "value":"'.$tribunal->getNomTribunal().'"}');
+            }
+            return $response;
+        }        
+        return $this->render('stage/popTribunal.html.twig', 
+            ['form' => $form->createView()
+            ]);
+    }
+
+     /**
+     *  @Route("/stage/loadFormPrefecture", name="stage_prefecture")
+     */
+    public function popPrefecture(Prefecture $prefecture = null, Request $request, ObjectManager $manager)
+    {
+        if(!$prefecture){
+            $prefecture = new Prefecture();
+        }
+
+        $form = $this->createForm( PrefectureType::class, $prefecture, array('method'=>'POST'));
+        $form->handleRequest( $request );
+
+        if($request->isMethod('POST')){
+          
+            $prefectureNom = $request->request->get('prefecture_nomPrefecture');
+            $prefectureAdresse = $request->request->get('prefecture_adressePrefecture');
+            $prefectureNumeroAdresse = $request->request->get('prefecture_numeroAdressePrefecture');
+            $prefectureCommune = $request->request->get('prefecture_communePrefecture');
+            $prefectureCP = $request->request->get('prefecture_cpPrefecture');
+            $prefectureAutorite = $request->request->get('prefecture_prefectureAutorite');
+            $prefectureService = $request->request->get('prefecture_prefectureService');
+            
+            // dump($request->request->get('prefecture_prefectureAutorite'));
+            // die();
+                if(strlen($prefectureNom) > 0 && strlen($prefectureAdresse) > 0 && strlen($prefectureNumeroAdresse) > 0 &&
+                    strlen($prefectureCommune) > 0 && strlen($prefectureCP) > 0 && strlen($prefectureAutorite) > 0 &&
+                    strlen($prefectureService) > 0){
+
+                    $prefecture->setNomPrefecture($prefectureNom);
+                    $prefecture->setAdressePrefecture($prefectureAdresse);
+                    $prefecture->setNumeroAdressePrefecture($prefectureNumeroAdresse);
+                    $prefecture->setCpPrefecture($prefectureCP);
+                    $prefecture->setCommunePrefecture($prefectureCommune);
+                    $prefecture->getPrefectureService($prefectureAutorite);
+                    $prefecture->getPrefectureAutorite($prefectureService);
+                    $manager->persist($prefecture);
+                    $manager->flush();
+                   
+                    $response = new Response();
+                    $response = JsonResponse::fromJsonString('{"id":'.$prefecture->getId().', "value":"'.$prefecture->getNomPrefecture().'"}');
+                }
+            return $response;
+        }        
+        return $this->render('stage/popPrefecture.html.twig', 
+            ['form' => $form->createView()
+            ]);
+    }
+
+    /**
      *  @Route("/stage/ajouter", name="stage_ajouter")
      *  @Route("/stage/{id}/modifier", name="stage_modifier")
      */
@@ -67,8 +167,6 @@ class StageController extends AbstractController
             'stage' => $stage
         ]);
     }
-
-
 }
 
 
