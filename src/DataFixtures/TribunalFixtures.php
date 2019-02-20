51ce970732bae9f6cd1cbe9d231c2e17529f428e
elodie-martin <elodie.m@codeur.online>
2019-02-20 10:41:51 +0000

    correction routes dans les controller et vues

--- src/DataFixtures/TribunalFixtures.php
+++ src/DataFixtures/TribunalFixtures.php
@@ -15,18 +15,21 @@ class TribunalFixtures extends Fixture
     {
         $faker = \Faker\Factory::create('fr_FR');
 
-        for ($j = 1; $j < 5; $j++) { 
+        for ($j = 1; $j < 3; $j++) { 
             $service = new TribunalService();
             $service->setNom($faker->word);
             $manager->persist($service);
-        
-            for ($k = 1; $k < 5; $k++) { 
-                $autorite = new TribunalAutorite();
-                $autorite->setNom($faker->word);
-                $manager->persist($autorite);
-            }
-       
-            for($i = 1; $i < 3; $i++){
+            $manager->flush();
+        }
+
+        for ($k = 1; $k < 3; $k++) { 
+            $autorite = new TribunalAutorite();
+            $autorite->setNom($faker->word);
+            $manager->persist($autorite);
+            $manager->flush();
+        }
+   
+        for($i = 1; $i < 3; $i++){
             $tribunal = new Tribunal();
             
             $tribunal->setNomTribunal("Tribunal n°$i");
@@ -37,8 +40,8 @@ class TribunalFixtures extends Fixture
             $tribunal->setTribunalService($service);
 
             $manager->persist($tribunal);
-            }
+            $manager->flush();
         }
-        $manager->flush();
+
     }
 }
